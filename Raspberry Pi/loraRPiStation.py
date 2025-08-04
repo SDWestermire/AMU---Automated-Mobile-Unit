a19129404c0ef1b670e25c338235ec2d3757241a
Steven D. Westermire <minako.steve@gmail.com>
2025-08-04 09:00:26 -0700

    Updates for 4 Aug

--- Raspberry Pi/loraRPiStation.py
+++ Raspberry Pi/loraRPiStation.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-# noted from https://github.com/papercodeIN/Embedded_Devices/blob/main/REYAX/RYLR998/Python/RYLR998%20Integration%20with%20Python.ipynb
+#name:  loraRPiStation.py 
+#noted from https://github.com/papercodeIN/Embedded_Devices/blob/main/REYAX/RYLR998/Python/RYLR998%20Integration%20with%20Python.ipynb
 # pip3 install pyserial
 #TESTED GOOD ON 2 JUL 2025
 import serial
@@ -33,7 +33,7 @@ def check_address():
     print(response)
 
 def set_address():
-    lora.write(b'AT+ADDRESS=2\r\n')
+    lora.write(b'AT+ADDRESS=1\r\n')
     time.sleep(1)
     response = lora.read(lora.inWaiting()).decode()
     print(response)
@@ -43,10 +43,9 @@ def check_lastpayload():
     time.sleep(0.1)
     response = lora.read(lora.inWaiting()).decode()
     print(response)
-
     
 def send_payload(loc):
-    lora.write(f'AT+SEND=1,{len(loc)},{loc}\r\n'.encode('utf-8'))
+    lora.write(f'AT+SEND=2,{len(loc)},{loc}\r\n'.encode('utf-8'))
     time.sleep(1)
     response = lora.read(lora.inWaiting()).decode()
     print(response)
@@ -85,95 +84,4 @@ check_rfband()
 
 monitor_serial_data(lora)
 
-
 lora.close
-=======
-# noted from https://github.com/papercodeIN/Embedded_Devices/blob/main/REYAX/RYLR998/Python/RYLR998%20Integration%20with%20Python.ipynb
-# pip3 install pyserial
-#TESTED GOOD ON 2 JUL 2025
-import serial
-import time
-import random
-from datetime import datetime
-
-# Initialize LoRa loraial communication
-lora = serial.Serial("/dev/ttyS0", 115200, timeout=1)
-lora.write(b"AT+NETWORKID=6")
-
-loc = str("33.6865105 -117.7923516")
-
-#loc = str("Hello")
-def check_networkid():
-    lora.write(b'AT+NETWORKID?\r\n')
-    time.sleep(1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-    
-def check_rfband():
-    lora.write(b'AT+BAND?\r\n')
-    time.sleep(1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-    
-def check_address():
-    lora.write(b'AT+ADDRESS?\r\n')
-    time.sleep(1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-
-def set_address():
-    lora.write(b'AT+ADDRESS=2\r\n')
-    time.sleep(1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-    
-def check_lastpayload():
-    lora.write(b'AT+SEND?\r\n')
-    time.sleep(0.1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-
-    
-def send_payload(loc):
-    lora.write(f'AT+SEND=1,{len(loc)},{loc}\r\n'.encode('utf-8'))
-    time.sleep(1)
-    response = lora.read(lora.inWaiting()).decode()
-    print(response)
-
-def monitor_serial_data(lora):
-    try:
-        print(f"Listening on {lora.port} at {lora.baudrate} baud rate...")
-
-        while True:
-            
-            if lora.in_waiting > 0:
-                data = lora.readline().decode('utf-8').strip()  # Read and decode data
-                if data:  # Ensure it's not empty
-                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get current timestamp
-                    # print(f"[{current_time}] Received data: {data}")
-                    
-                    if data.startswith("+RCV="):
-                        parts = data.split(',')
-                        node_id, msg_len, message, rssi, snr = parts[0], parts[1], parts[2], parts[3], parts[4]
-                        print(f"[{current_time}] Node ID: {node_id}, Message: {message}, RSSI: {rssi}, SNR: {snr}")
-                     
-            time.sleep(0.1)  # Prevent CPU overload
-            check_lastpayload()
-            send_payload(loc)
-    except KeyboardInterrupt:
-        print("\nProgram stopped by user.")
-    except serial.SerialException as e:
-        print(f"Serial Error: {e}")
-    finally:
-        print("Serial monitoring stopped.")
-
-check_networkid()
-set_address()
-check_address()
-check_rfband()
-
-monitor_serial_data(lora)
-
-
-lora.close
->>>>>>> b41c589df74dea4485edbb5110422e5f40bb8565
